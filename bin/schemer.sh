36d585064572a738cf85b7da0c30e08f59de633a
Lazar Bodor <lazar.bodor@ga.gov.au>
2016-01-28 10:12:30 +1100

    Pass http proxy info from the environment to java

--- bin/schemer.sh
+++ bin/schemer.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
 JAR=`dirname $0`/../lib/xml-schemer-jar-with-dependencies.jar
-java -jar ${JAR} $@
+
+if [ -n "${http_proxy}" ]; then
+    HOST_AND_PORT=$(echo ${http_proxy} | cut -f 3 -d\/)
+    PROXY_HOST=$(echo ${HOST_AND_PORT} | cut -f 1 -d:)
+    PROXY_PORT=$(echo ${HOST_AND_PORT} | cut -f 2 -d:)
+    PROXY_FOR_JAVA="-Dhttp.proxyHost=${PROXY_HOST} -Dhttp.proxyPort=${PROXY_PORT}"
+fi
+
+java ${PROXY_FOR_JAVA} -jar ${JAR} $@
 
