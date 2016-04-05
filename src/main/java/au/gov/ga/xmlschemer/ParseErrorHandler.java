bc15a89e196b7dbbf0b75c3f5971f91885fed2bf
lbodor <lazar.bodor@gmail.com>
2016-04-05 19:52:01 +1000

    Use slf4j, not System.out.println

--- src/main/java/au/gov/ga/xmlschemer/ParseErrorHandler.java
+++ src/main/java/au/gov/ga/xmlschemer/ParseErrorHandler.java
@@ -3,10 +3,14 @@ package au.gov.ga.xmlschemer;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.xml.sax.ErrorHandler;
 import org.xml.sax.SAXParseException;
 
 public class ParseErrorHandler implements ErrorHandler {
+
+    private static final Logger log = LoggerFactory.getLogger(ParseErrorHandler.class);
 
     private List<String> violations = new ArrayList<String>();
 
@@ -15,12 +19,11 @@ public class ParseErrorHandler implements ErrorHandler {
     }
 
     public void fatalError(SAXParseException e) {
-        System.out.println("FATAL");
         violations.add(prepareMessage(e));
     }
 
     public void warning(SAXParseException e) {
-        System.out.println(prepareMessage(e));
+        log.warn(prepareMessage(e));
     }
 
     public boolean hasErrors() {
