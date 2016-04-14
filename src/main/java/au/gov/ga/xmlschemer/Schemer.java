187e09480710fbc6cd70569e1b6126c2643043ef
Lazar Bodor <lazar.bodor@ga.gov.au>
2016-04-14 13:08:02 +1000

    Use jcommander to also run schematron validation

--- src/main/java/au/gov/ga/xmlschemer/Schemer.java
+++ src/main/java/au/gov/ga/xmlschemer/Schemer.java
@@ -1,77 +1,96 @@
 package au.gov.ga.xmlschemer;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.util.List;
 
 import javax.xml.transform.Source;
 import javax.xml.transform.stream.StreamSource;
 
-import org.apache.commons.cli.CommandLine;
-import org.apache.commons.cli.CommandLineParser;
-import org.apache.commons.cli.DefaultParser;
-import org.apache.commons.cli.HelpFormatter;
-import org.apache.commons.cli.Option;
-import org.apache.commons.cli.Options;
-import org.apache.commons.cli.ParseException;
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
+import com.beust.jcommander.Parameters;
 
 public class Schemer {
 
+    private static final String schemaCommandName = "schema";
+    private static final String schematronCommandName = "schematron";
+
+    private static class GlobalOptions {
+        @Parameter(names = {"-h", "--help"}, description = "Print usage", help = true)
+        public boolean help;
+    }
+
+    private static class CommandOptions {
+        @Parameter(names = "--xml", description = "XML file to validate", arity = 1, required = true)
+        public String xmlFileName;
+
+        @Parameter(names = "--catalog", description = "OASIS catalog file", arity = 1)
+        public String catalogFileName;
+    }
+
+    @Parameters(commandDescription = "Run schema validation")
+    private static class SchemaCommandOptions extends CommandOptions {
+        @Parameter(names = "--xsd", description = "XSD schema file", arity = 1, required = true)
+        public String xsdFileName;
+    }
+
+    @Parameters(commandDescription = "Run schematron validation")
+    private static class SchematronCommandOptions extends CommandOptions {
+        @Parameter(names = "--xslt", description = "Schematron XSLT file", arity = 1, required = true)
+        public String xsltFileName;
+    }
+
     public static void main(String[] args) {
-        Option xsdFileOption = Option.builder(null)
-            .type(String.class)
-            .argName("xsd-file")
-            .longOpt("xsd")
-            .required()
-            .hasArg()
-            .desc("XSD file")
-            .build();
 
-        Option xmlFileOption = Option.builder(null)
-            .type(String.class)
-            .argName("xml-file")
-            .longOpt("xml")
-            .required()
-            .hasArg()
-            .desc("XML file")
-            .build();
-
-        Option catalogFileOption = Option.builder(null)
-            .type(String.class)
-            .longOpt("catalog")
-            .argName("oasis-catalog-file")
-            .hasArg()
-            .desc("schema catalog file")
-            .build();
-
-        Options options = new Options();
-        options.addOption(catalogFileOption);
-        options.addOption(xsdFileOption);
-        options.addOption(xmlFileOption);
-
-        CommandLineParser parser = new DefaultParser();
-
+        GlobalOptions globalOptions = new GlobalOptions();
+        JCommander commander = new JCommander(globalOptions);
+        commander.setProgramName("schemer.sh");
+        SchemaCommandOptions schemaCommandOptions = new SchemaCommandOptions();
+        commander.addCommand(schemaCommandName, schemaCommandOptions);
+        SchematronCommandOptions schematronCommandOptions = new SchematronCommandOptions();
+        commander.addCommand(schematronCommandName, schematronCommandOptions);
         try {
-            CommandLine line = parser.parse(options, args);
-            Source xsdFile = new StreamSource(line.getOptionValue("xsd"));
-            Source xmlFile = new StreamSource(line.getOptionValue("xml"));
-            String catalogFile = line.getOptionValue("catalog");
-
-            SchemaValidator schemaValidator = new SchemaValidator(xsdFile, catalogFile);
-            List<String> violations = schemaValidator.validate(xmlFile);
-
-           if (!violations.isEmpty()) {
-                for (String violation : violations) {
-                    System.err.println(violation);
-                }
-                System.exit(1);
-            }
+            commander.parse(args);
         }
-        catch (ParseException e) {
+        catch (ParameterException e) {
             System.err.println(e.getMessage());
-            new HelpFormatter().printHelp(120, "schemer.sh", "", options, "", true);
+            commander.usage();
             System.exit(1);
+        }
+        try {
+            if (globalOptions.help) {
+                commander.usage();
+            } else if (schemaCommandName.equals(commander.getParsedCommand())) {
+                Source xsdFile = new StreamSource(schemaCommandOptions.xsdFileName);
+                Source xmlFile = new StreamSource(schemaCommandOptions.xmlFileName);
+                String catalogFile = schemaCommandOptions.catalogFileName;
+
+                SchemaValidator schemaValidator = new SchemaValidator(xsdFile, catalogFile);
+                List<String> violations = schemaValidator.validate(xmlFile);
+
+                if (!violations.isEmpty()) {
+                    for (String violation : violations) {
+                        System.err.println(violation);
+                    }
+                    System.exit(1);
+                }
+            } else if (schematronCommandName.equals(commander.getParsedCommand())) {
+                Source xsltFile = new StreamSource(schematronCommandOptions.xsltFileName);
+                Source xmlFile = new StreamSource(schematronCommandOptions.xmlFileName);
+                String catalogFile = schematronCommandOptions.catalogFileName;
+
+                SchematronValidator schematronValidator = new SchematronValidator(xsltFile, catalogFile);
+                List<String> violations = schematronValidator.validate(xmlFile);
+
+                if (!violations.isEmpty()) {
+                    for (String violation : violations) {
+                        System.err.println(violation);
+                    }
+                    System.exit(1);
+                }
+            } else {
+                commander.usage();
+            }
         }
         catch (Exception e) {
             e.printStackTrace();
