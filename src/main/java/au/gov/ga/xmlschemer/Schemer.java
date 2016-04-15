a380e3d466076a97daa0c937345b36f401cd624c
lbodor <lazar.bodor@gmail.com>
2016-04-15 11:54:47 +1000

    Validate file parameters

--- src/main/java/au/gov/ga/xmlschemer/Schemer.java
+++ src/main/java/au/gov/ga/xmlschemer/Schemer.java
@@ -1,16 +1,22 @@
 package au.gov.ga.xmlschemer;
 
+import java.io.File;
 import java.util.List;
 
 import javax.xml.transform.Source;
 import javax.xml.transform.stream.StreamSource;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.beust.jcommander.IParameterValidator;
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
 import com.beust.jcommander.ParameterException;
 import com.beust.jcommander.Parameters;
 
 public class Schemer {
+    private static final Logger log = LoggerFactory.getLogger(Schemer.class);
 
     private static final String schemaCommandName = "schema";
     private static final String schematronCommandName = "schematron";
@@ -21,22 +27,22 @@ public class Schemer {
     }
 
     private static class CommandOptions {
-        @Parameter(names = "--xml", description = "XML file to validate", arity = 1, required = true)
+        @Parameter(names = "--xml", description = "XML file to validate", arity = 1, required = true, validateWith = FileValidator.class)
         public String xmlFileName;
 
-        @Parameter(names = "--catalog", description = "OASIS catalog file", arity = 1)
+        @Parameter(names = "--catalog", description = "OASIS catalog file", arity = 1, validateWith = FileValidator.class)
         public String catalogFileName;
     }
 
     @Parameters(commandDescription = "Run schema validation")
     private static class SchemaCommandOptions extends CommandOptions {
-        @Parameter(names = "--xsd", description = "XSD schema file", arity = 1, required = true)
+        @Parameter(names = "--xsd", description = "XSD schema file", arity = 1, required = true, validateWith = FileValidator.class)
         public String xsdFileName;
     }
 
     @Parameters(commandDescription = "Run schematron validation")
     private static class SchematronCommandOptions extends CommandOptions {
-        @Parameter(names = "--xslt", description = "Schematron XSLT file", arity = 1, required = true)
+        @Parameter(names = "--xslt", description = "Schematron XSLT file", arity = 1, required = true, validateWith = FileValidator.class)
         public String xsltFileName;
     }
 
@@ -53,8 +59,10 @@ public class Schemer {
             commander.parse(args);
         }
         catch (ParameterException e) {
-            System.err.println(e.getMessage());
-            commander.usage();
+            log.error(e.getMessage());
+            if (!(e instanceof ParameterValidationException)) { 
+                commander.usage();
+            }
             System.exit(1);
         }
         try {
@@ -77,9 +85,10 @@ public class Schemer {
             } else if (schematronCommandName.equals(commander.getParsedCommand())) {
                 Source xsltFile = new StreamSource(schematronCommandOptions.xsltFileName);
                 Source xmlFile = new StreamSource(schematronCommandOptions.xmlFileName);
-                String catalogFile = schematronCommandOptions.catalogFileName;
-
-                SchematronValidator schematronValidator = new SchematronValidator(xsltFile, catalogFile);
+                if (!new File(schematronCommandOptions.catalogFileName).exists()) {
+                    abort("Catalog file " + schematronCommandOptions.catalogFileName + " does not exist.");
+                }
+                SchematronValidator schematronValidator = new SchematronValidator(xsltFile, schematronCommandOptions.catalogFileName);
                 List<String> violations = schematronValidator.validate(xmlFile);
 
                 if (!violations.isEmpty()) {
@@ -92,10 +101,28 @@ public class Schemer {
                 commander.usage();
             }
         }
-        catch (Exception e) {
-            e.printStackTrace();
+        catch (Throwable e) {
+            log.error("Unexpected error", e);
             System.exit(1);
         }
     }
-}
 
+    public static class FileValidator implements IParameterValidator {
+        public void validate(String name, String value) throws ParameterException {
+            if (!new File(value).exists()) {
+                throw new ParameterValidationException("File " + value + " does not exist.");
+            }
+        }
+    }
+
+    public static class ParameterValidationException extends ParameterException {
+        public ParameterValidationException(String message) {
+            super(message);
+        }
+    }
+
+    private static void abort(String message) {
+        log.error(message);
+        System.exit(1);
+    }
+}
